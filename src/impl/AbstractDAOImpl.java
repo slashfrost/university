a4e26391873e683d92769d1734c757b605471a41
slashfrost <morozalexander82@gmail.com>
2018-10-10 00:05:52 +0300

    DAO:добавил генерики и метод поиска по id

--- src/impl/AbstractDAOImpl.java
+++ src/impl/AbstractDAOImpl.java
@@ -7,23 +7,33 @@ import model.BaseModel;
 
 import java.util.List;
 
-public abstract class AbstractDAOImpl extends BaseModel implements BaseDAO {
-    List<BaseModel> list;
+public abstract class AbstractDAOImpl <T extends BaseModel >implements BaseDAO<T> {
+   protected List<T> list;
     @Override
     public boolean create(BaseModel entity) {
         //???
     }
 
     @Override
-    public boolean delete(BaseModel entity) {
+    public boolean delete(T entity) {
         return list.remove(entity);
     }
 
     @Override
-    public boolean update(BaseModel entity) {
+    public boolean update(T entity) {
         return list.add(entity);
     }
+    @Override
+    public T get(Long id) {
+        for (T e : list) {
+            if (e.getId().equals(id)) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
+
 
 
 
